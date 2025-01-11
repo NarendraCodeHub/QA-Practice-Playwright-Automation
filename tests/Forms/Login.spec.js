5f4e3203e7ea141c81b3814c03079052916b7aae
Narendra122812 <narendrakumar122812@gmail.com>
2025-01-12 03:38:36 +0530

    Added Login Form Test Script

--- tests/Forms/Login.spec.js
+++ tests/Forms/Login.spec.js
@@ -6,3 +6,52 @@ test('Login - Shop',async({page}) => {
     // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle('QA Practice | Learn with RV')
 });
+
+test('check-form-validation', async ({ page }) => {
+    await page.goto('https://qa-practice.netlify.app/auth_ecommerce');
+
+    // Check form validation without entering any fields & click on submit
+    await page.getByRole('button', { name: 'Submit' }).click();
+
+    const errorMessage = await page.getByText('Bad credentials! Please try');
+
+    await expect(errorMessage).toBeVisible();
+
+    await expect(errorMessage).toHaveText("Bad credentials! Please try again! Make sure that you've registered.");
+});
+
+test('check-form-validation-for-invalid-user-or-password', async ({ page }) => {
+
+    await page.goto('https://qa-practice.netlify.app/auth_ecommerce');
+
+    await page.locator('#email').fill('test@gmail.com');
+
+    await page.locator('#password').fill('test@123');
+
+    await page.getByRole('button', { name: 'Submit' }).click();
+
+    const errorMessage = await page.getByText('Bad credentials! Please try');
+
+    await expect(errorMessage).toBeVisible();
+
+    await expect(errorMessage).toHaveText("Bad credentials! Please try again! Make sure that you've registered.");
+
+});
+
+test('check-form-for-valid-user-or-password', async ({ page }) => {
+
+    await page.goto('https://qa-practice.netlify.app/auth_ecommerce');
+
+    await page.locator('#email').fill('admin@admin.com');
+
+    await page.locator('#password').fill('admin123');
+
+    await page.getByRole('button', { name: 'Submit' }).click();
+
+    const shoppingCart = await page.locator('text=SHOPPING CART'); 
+
+    await expect(shoppingCart).toBeVisible();
+
+    await expect(shoppingCart).toHaveText("SHOPPING CART");
+
+});
